7355512870e16c3d61c0d9faa9d60eba29ddc29d
thiruvenkatasamyiyya <thiruvenkatasamyiyya@gmail.com>
2025-05-20 20:17:37 +0530

    try10

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/",
-  publicDir: 'public', // makes sure public files get copied
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
+    outDir: 'dist',
   },
+  server: {
+    historyApiFallback: true,
+  }
 })
